408ad559207e4ed6544f05ff799a4aabed32d5d3
Dnyaneshwari <dalvidnyaneshwari25@gmail.com>
2026-03-10 02:33:56 +0530

    debug supabase env

--- supabase_client.py
+++ supabase_client.py
@@ -6,11 +6,11 @@ from dotenv import load_dotenv
 
 # load_dotenv()
 
-import os
-from supabase import create_client
-
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+
+print("SUPABASE_URL:", SUPABASE_URL)
+print("SUPABASE_KEY:", SUPABASE_KEY)
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise Exception("Supabase environment variables missing")
